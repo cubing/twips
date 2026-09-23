9ab9a66b15d18e6bb0f277c848024c344355d802
Lucas Garron <code@garron.net>
2026-09-22 17:48:04 -0700

    [read_to_json.rs] Print the parsing error.

--- src/lib/_internal/read_to_json.rs
+++ src/lib/_internal/read_to_json.rs
@@ -6,7 +6,8 @@ use crate::_internal::errors::ArgumentError;
 
 pub fn read_to_json<T: for<'a> Deserialize<'a>>(input_file: &Path) -> Result<T, ArgumentError> {
     let input_str = read_to_string(input_file).or(Err("Could not read input file."))?;
-    let input_parsed: T =
-        serde_json::from_str(&input_str).or(Err("Input file is not valid JSON."))?;
+    let input_parsed: T = serde_json::from_str(&input_str).map_err(|e| ArgumentError {
+        description: format!("Could not parse input file: {}", e),
+    })?;
     Ok(input_parsed)
 }
